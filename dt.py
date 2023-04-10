66a431fa1840a2c7ad90eb02f0944e4dc87b3a5f
tunmx <tunmxy@163.com>
2023-04-10 17:28:49 +0800

    lmk

--- dt.py
+++ dt.py
@@ -3,6 +3,16 @@ import os
 import cv2
 import numpy as np
 
+def get_aligned_lmk(kps, mat):
+
+    ones = np.ones((kps.shape[0], 1))
+
+    lmk_h = np.concatenate((kps, ones), axis=1)
+
+    align_points = np.dot(mat, lmk_h.T).T
+
+    return align_points
+
 def trans_to_align(image, kps, input_size=256):
     from skimage import transform as trans
     new_size = 144
@@ -22,22 +32,31 @@ def trans_to_align(image, kps, input_size=256):
 
     return tform, warped
 
+input_size = 256
 item_data = "/Users/tunm/datasets/ballFaceDataset20230317-FMESH/1654053172386_whiteLeft_left"
-
 raw_image_path = os.path.sep.join([item_data, 'raw_image.jpg'])
-kps68_path = os.path.sep.join([item_data, 'kps68.npy'])
+trans_matrix_path = os.path.sep.join([item_data, 'to256_trans_matrix.npy'])
 mesh_path = os.path.sep.join([item_data, 'raw_mesh.npy'])
+kps5_path = os.path.sep.join([item_data, 'kps5.npy'])
+
+trans_matrix = np.load(trans_matrix_path)
+mesh = np.load(mesh_path)
+kps5 = np.load(kps5_path)
 
 raw_image = cv2.imread(raw_image_path)
-kps68 = np.load(kps68_path)
-mesh = np.load(mesh_path)
 
-for x, y in mesh.astype(int):
-    cv2.circle(raw_image, (x, y), 0, (0, 0, 255), 5)
+warped = cv2.warpAffine(raw_image, trans_matrix, (input_size, ) * 2, )
+warped_mesh = get_aligned_lmk(mesh, trans_matrix)
+warped_kps5 = get_aligned_lmk(kps5, trans_matrix)
 
-for x, y in kps68.astype(int):
-    cv2.circle(raw_image, (x, y), 0, (0, 255, 0), 10)
+for x, y in warped_mesh.astype(int):
+    cv2.circle(warped, (x, y), 0, (0, 0, 255), 1)
 
-cv2.imshow("w", raw_image)
+for x, y in warped_kps5.astype(int):
+    cv2.circle(warped, (x, y), 0, (0, 255, 0), 3)
+
+cv2.imshow("w", warped)
 cv2.waitKey(0)
+
+print(mesh.shape)
 
